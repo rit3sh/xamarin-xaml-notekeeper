1166f49cb0489b537d22c203170b8cefa0885000
Ritesh Shahi <ritesh.shahi@outlook.com>
2019-09-28 12:32:15 -0500

    Add project files.

---
+++ NoteKeeper/NoteKeeper/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using NoteKeeper.Models;
+using NoteKeeper.ViewModels;
+
+namespace NoteKeeper.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class ItemDetailPage : ContentPage
+    {
+        ItemDetailViewModel viewModel;
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+
+            var item = new Item
+            {
+                Text = "Item 1",
+                Description = "This is an item description."
+            };
+
+            viewModel = new ItemDetailViewModel(item);
+            BindingContext = viewModel;
+        }
+
+        private void Cancel_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Cancel Option", "Cancel was selected", "Button 2", "Button 1");
+        }
+
+        private void Save_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Save Option", "Save was selected", "Button 2", "Button 1");
+        }
+    }
+}
